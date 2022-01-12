e4f55640afd1f5ccff7cefdd0f5a3945732b8f07
Koen <2144264@talnet.nl>
2022-01-12 11:28:06 +0100

    now works when there is a negative but no positive. does not work when pair occurs when pair should be checked several times because all instances of number is deleted

--- jan12.php
+++ jan12.php
@@ -1,19 +1,15 @@
 <?php
-$input = [4, 3, 2, -2, -3, 5, 7];
-rsort($input);
-$result = [];
+$input = [4, 3, 2, -2, -3, 5, 7, -6];
+sort($input);
 
 foreach($input as $value) {
-    if ($value < 0) {break;}
+    if ($value > 0) {break;}
 
-    if (!array_search($value * -1, $input)) {
-        array_push($result, $value);
+    if (array_search($value * -1, $input)) {
+        $input = array_diff($input, [$value, $value * -1]);
     }
 }
 
-if (empty($result)) {echo "no result";} else {
-    foreach ($result as $value) {
-        echo $value . "<br>";
-    }
+foreach ($input as $value) {
+    echo $value . "<br>";
 }
-
